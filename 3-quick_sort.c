f313dcfc14d20fa80d7e334ca436abd00c1cd130
lily <hueduong288@gmail.com>
2025-04-12 22:42:13 +0000

    Write a function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,7 +12,7 @@ void swap(int *a, int *b)
 {
 	int temp = *a;
 	*a = *b;
-	*b=temp;
+	*b = temp;
 }
 
 /**
@@ -25,12 +25,13 @@ void swap(int *a, int *b)
  * Return: void
  */
 
-void lomuto_partition(int *array, size_t low, size_t high, size_t size)
+size_t lomuto_partition(int *array, size_t low, size_t high, size_t size)
 {
 	int pivot = array[high];
-	size_t i = low;
+	size_t i, j;
+	i = low;
 
-	for (size_t j = low; j < high -1; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] < pivot)
 		{
@@ -39,15 +40,54 @@ void lomuto_partition(int *array, size_t low, size_t high, size_t size)
 				swap(&array[i], &array[j]);
 				print_array(array, size);
 			}
+			i++;
 		}
-		i++;
+	
 	}
 
 	if (i != high)
 	{
-		swap(&array[i], &array[high];
+		swap(&array[i], &array[high]);
 		print_array(array, size);
 	}
 
 	return i;
 }
+
+/**
+ * quick_sort_recursive - recursively apply quick_sort to subarrays
+ * @array: pointer to the array
+ * @low: the starting index of current array
+ * @high: the ending index of current array
+ * @size: the number of elements in array
+ * Return: void
+ */
+
+void quick_sort_recursive(int *array, size_t low, size_t high, size_t size)
+{
+	if (low < high)
+	{
+		size_t pi = lomuto_partition(array, low, high, size);
+		/* the index where pivot is at the correct position */
+
+		if (pi > 0) /* to avoid negative number with pi - 1 */
+			quick_sort_recursive(array, low, pi - 1, size);
+
+		quick_sort_recursive(array, pi + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - to sort array in ascending order using Quick sort algorithm
+ * @array: the pointer to the array
+ * @size: the number of elements in the array.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
+}
+
